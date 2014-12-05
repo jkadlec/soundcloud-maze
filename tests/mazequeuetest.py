eba506de299f116a10bbfb771e7392e220c45ada
Jan Kadlec <jan.kadlec.cz@gmail.com>
2014-12-06 00:29:16 +0100

    unittests: simplified queue tests, removed MazeEvent dependency

--- tests/mazequeuetest.py
+++ tests/mazequeuetest.py
@@ -3,33 +3,33 @@
 # A fitting copyright should be put here.
 
 from maze.mazequeue import *
-from maze.mazeevent import * #TODO no-go
 import unittest, unittest.mock
 
 Q_SIZE = 8
 
-def create_payload(seq):
-    return "".join(str(seq) + "|P|1|" + str(seq) + "\n").encode()
+def create_dispatch(seq_nr, dispatched):
+    return lambda x, y: dispatched.append(seq_nr)
 
 class TestQueue(unittest.TestCase):
     def test_order(self):
         q = MazeQueue(Q_SIZE)
 
-        # create private messages with the same seq as destination, real events are easier TODO for now
         order = [4, 5, 3, 1, 2, 6, 7, 8, 12, 11, 10, 9]
-        fc = MazeEventFactory()
-        events = [ fc.create_event_from(create_payload(seq)) for seq in order ]
         batch_sizes = [5, 1, 1, 1, 4]
-        users = { i:unittest.mock.Mock() for i in range(13) }
+
+        dispatched = []
+        events = [ unittest.mock.Mock(\
+                   seq = seq_nr,\
+                   dispatch = create_dispatch(seq_nr, dispatched)) for seq_nr in order ]
         for batch_size in batch_sizes:
-            added = []
+            dispatched.clear()
             for _ in range(batch_size):
                 ev = events.pop(0)
-                q.add_and_process(ev, None, users)
-                added.append(ev.seq)
-            # check that whole batch has been called
-            for seq in added:
-                users[seq].write.assert_called_with(create_payload(seq))
+                q.add_and_process(ev, None, None)
+            # check that whole batch has been dispatched
+            self.assertEqual(len(dispatched), batch_size)
+            # check that the order was correct
+            self.assertEqual(dispatched, sorted(dispatched))
 
     def test_overfill(self):
         q = MazeQueue(4)
