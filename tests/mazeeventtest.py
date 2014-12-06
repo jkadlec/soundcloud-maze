c09c2496e4262cb00bbfebd3b0c9c589c1ba1bb4
Jan Kadlec <jan.kadlec.cz@gmail.com>
2014-12-06 04:30:25 +0100

    unittests: used IsInstance() instead of IsTrue(isinstance())

--- tests/mazeeventtest.py
+++ tests/mazeeventtest.py
@@ -27,31 +27,31 @@ class TestEvent(unittest.TestCase):
         # create follow event
         payload = b"123|F|1|2\n"
         ev = factory.create_event_from(payload)
-        self.assertTrue(isinstance(ev, FollowEvent))
+        self.assertIsInstance(ev, FollowEvent)
         self.assertTrue(check_attributes(ev, 123, payload, 1, 2))
 
         # create unfollow event
         payload = b"124|U|5|6\n"
         ev = factory.create_event_from(payload)
-        self.assertTrue(isinstance(ev, UnfollowEvent))
+        self.assertIsInstance(ev, UnfollowEvent)
         self.assertTrue(check_attributes(ev, 124, payload, 5, 6))
 
         # create broadcast event
         payload = b"125|B\n"
         ev = factory.create_event_from(payload)
-        self.assertTrue(isinstance(ev, BroadcastEvent))
+        self.assertIsInstance(ev, BroadcastEvent)
         self.assertTrue(check_attributes(ev, 125, payload))
 
         # create private message event
         payload = b"126|P|11|12\n"
         ev = factory.create_event_from(payload)
-        self.assertTrue(isinstance(ev, MessageEvent))
+        self.assertIsInstance(ev, MessageEvent)
         self.assertTrue(check_attributes(ev, 126, payload, dst=12))
 
         # create status update event
         payload = b"127|S|16\n"
         ev = factory.create_event_from(payload)
-        self.assertTrue(isinstance(ev, StatusEvent))
+        self.assertIsInstance(ev, StatusEvent)
         self.assertTrue(check_attributes(ev, 127, payload, 16))
 
     def test_factory_invalid(self):
