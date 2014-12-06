3f4d4e617ac5f16c116ae629b00c79e7b1593de3
Jan Kadlec <jan.kadlec.cz@gmail.com>
2014-12-06 02:08:53 +0100

    doc: added docstrings for MazeServer

--- maze/mazeserver.py
+++ maze/mazeserver.py
@@ -4,6 +4,17 @@ import maze.mazequeue as mazequeue
 import maze.mazeevent as mazeevent
 
 class MazeServer(object):
+
+    """A Follower Maze server. Handles incoming events and active users.
+
+       Attributes:
+           conf:       server configuration, instance of MazeConfig
+           users:      dictionary with active users, values are asyncio transports
+           followers:  dictionary with users followers, values are dictionaries with following users.
+           factory:    factory used to create MazeEvent objects from payloads.
+           q:          queue used for correct order processing and event dispatching.
+    """
+
     def _setup(self, conf):
         self.conf = conf
         self.users = {}
@@ -12,9 +23,19 @@ class MazeServer(object):
         self.q = mazequeue.MazeQueue(conf.q_size)
 
     def __init__(self, conf):
-        self._setup(conf)
+        """Inits server using given configuration, internal structures are created.
 
+           Args:
+               conf:  parsed MazeConfig configuration
+        """
+        self._setup(conf)
     def process_events(self, data):
+        """Processes incoming stream of events, stores them into server queue.
+           Will also dispatch events from queue if they are in correct order after event addtion.
+
+           Args:
+               data:  stream of encoded data, delimited with \n or \r\n.
+        """
         sp = data.splitlines(True)
         for line in sp:
             try:
@@ -26,15 +47,28 @@ class MazeServer(object):
                 pass
 
     def add_client(self, transport, data):
+        """Adds a new client's transport object into server user dictionary.
+
+           Args:
+               transport:  asyncio.transport connected to the client
+               data:       data containing user id
+        """
         sp = data.splitlines()
         client_id = int(sp[0])
         self.users[client_id] = transport
         return client_id
 
     def remove_client(self, client_id):
+        """Removes a client from server user dictionary.
+
+           Args:
+               client_id:  id of client to be removed
+        """
         if client_id in self.users:
             self.users.pop(client_id)
 
     def reset(self):
+        """Resets all the server structures.
+        """
         self._setup(self.conf)
 
