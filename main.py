9b9643de1e765ca5de039b27142698c7848df7dd
Jan Kadlec <jan.kadlec.cz@gmail.com>
2014-12-06 04:46:59 +0100

    main: added a brief help message

--- main.py
+++ main.py
@@ -7,8 +7,20 @@ import maze.mazeconfig as mazeconfig
 import maze.mazeserver as mazeserver
 import maze.mazeprotocol as mazeprotocol
 
+def help():
+    print("USAGE: ./main.py -c configuration_file\n\n"
+          "minimal configuration file could look like this:\n\n"
+          "[interface]\n"
+          "event_address = 127.0.0.1\n"
+          "event_port = 9090\n"
+          "client_address = 127.0.0.1\n"
+          "client_port = 9099\n"
+          "[system]\n"
+          "queue_size = 1048576\n")
+
 def main():
     if not len(sys.argv) == 3:
+        help()
         sys.exit(1)
 
     with open(sys.argv[2], "r") as f:
